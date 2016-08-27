b2e7d39ca5c1f7d6c86050485268386a9354fe93
estrattonbailey <ericstrattonbailey@gmail.com>
2016-08-27 17:12:46 -0400

    Add support for nested element arrays

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ const h = tag => (...args) => isProps(args[0])
   ? applyProps(tag)(args[0])
   : appendChildren(tag)(...args)
 
-const isObj = o => o !== null && typeof o === 'object'
+const isArr = o => Array.isArray(o)
+
+const isObj = o => o !== null && typeof o === 'object' && !isArr(o)
 
 const isProps = arg => isObj(arg) && !(arg instanceof Element)
 
@@ -26,8 +28,19 @@ const applyProps = tag => props => (...args) => {
   return el
 }
 
+const walkArray = (arr) => {
+  let res
+  const walk = arr => {
+    res = arr
+    arr.map(c => isArr(c) ? walk(c) : res)
+  }
+  walk(arr)
+  return res 
+} 
+
 const appendChildren = tag => (...children) => {
   const el = document.createElement(tag)
+  children = isArr(children) && isArr(children[0]) ? walkArray(children) : children
   children.map(c => c instanceof Element ? c : document.createTextNode(c))
     .forEach(c => el.appendChild(c))
   return el
